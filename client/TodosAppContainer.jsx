ac2c3fc553d09db80f4d1dd755fcb458be6ca2f2
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2016-04-01 11:05:01 +0200

    removed unnecessary onTodoClick

--- client/TodosAppContainer.jsx
+++ client/TodosAppContainer.jsx
@@ -4,17 +4,15 @@ import { TodosApp }              from './TodosApp.jsx';
 import { store }              from './main.jsx';
 
 function composer( props, onData ) {
-    const onTodoClick = id => store.dispatch({ type: 'TOGGLE_TODO', id });
     onData(
       null,
       {
         ...store.getState(),
-        onTodoClick
       }
     );
 
     return store.subscribe(() => {
-      onData( null, { ...store.getState(), onTodoClick } ) // spreads the store into todos and visibilityFilter
+      onData( null, { ...store.getState() } ) // spreads the store into todos and visibilityFilter
     });
 };
 
